8a4d2a7ff2bbfc1fb7f2bc20cd8d31885f1e1cd2
cjun2351 <cjun2351@gmail.com>
2024-01-30 21:19:42 +0800

    改小東西

--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -2,7 +2,7 @@
   <main class="member">
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb">
-        <li class="breadcrumb-item active" aria-current="page">
+        <li class="breadcrumb-item" aria-current="page">
           <router-link to="/">首頁</router-link>
         </li>
         <li class="breadcrumb-item active" aria-current="page">會員管理</li>
