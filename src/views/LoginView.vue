6c9caa5259e3efdad61b257f4dbf9cbade1b8947
martina216 <10230260c@gmail.com>
2024-01-30 21:13:07 +0800

    建立登入頁面，連接首頁

---
+++ src/views/LoginView.vue
@@ -0,0 +1,39 @@
+<script></script>
+
+<template>
+  <main class="login">
+    <div class="wrrap">
+      <div class="logo">
+        <img src="@/assets/images/login/Grid Island.svg">
+      </div>
+      <span class="pc-h3">後台登入</span>
+      <form method="post">
+        <input type="text" class="form-control user" id="exampleInput" placeholder="請輸入使用者帳號">
+        <input type="password" class="form-control" id="exampleInput" placeholder="請輸入使用者密碼">
+        <button type="button" class="btn btn-light rounded-pill padding" @click="login">登入</button>
+      </form>
+    </div>
+  </main>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    login() {
+      this.$router.push('/');
+    },
+  },
+  created() {
+  },
+  methods: {
+  },
+  mounted() {},
+};
+</script>
+
