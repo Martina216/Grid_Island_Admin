65789a498c753f8c1f734367fafbb63d0111c503
cjun2351 <cjun2351@gmail.com>
2024-01-30 17:02:15 +0800

    增加 SideNavBar 跟 NavBar 以及頁面 padding 設定

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home">
+    <h3>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, odio!
+      Sapiente sit iste cum rem voluptatum laboriosam animi aut illo delectus.
+      Nemo sit facere ipsum voluptatum nesciunt tempore eius explicabo
+      temporibus animi atque nihil facilis consectetur soluta labore ex neque
+      nam, accusantium ut itaque mollitia? Aspernatur ullam alias velit
+      suscipit.
+    </h3>
   </main>
 </template>
---
+++ src/views/MemberView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="member">
+    <h3>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+      consequuntur accusantium nulla ut numquam inventore id hic quidem, quaerat
+      ratione maiores earum, sequi at voluptatibus accusamus libero quasi beatae
+      voluptas ullam. Voluptas, ad. Odio dolor in recusandae quaerat iste, rem,
+      doloremque accusamus, voluptate amet corrupti asperiores. Distinctio
+      ducimus, doloribus odit omnis, molestiae, beatae vel consequuntur sint
+      eaque voluptatum nulla. Ullam sequi iure corrupti, nisi ipsa cumque
+      accusantium accusamus illum consequatur rerum quod voluptatem recusandae
+      nulla? Eaque necessitatibus cum vero corrupti, nobis autem quaerat quasi
+      dolorum voluptas explicabo voluptatum illo reprehenderit quod aliquam.
+      Mollitia a reiciendis ullam nobis amet. Velit, suscipit.
+    </h3>
+  </div>
+</template>
