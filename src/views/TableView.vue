2b34ca766ffd735a3e28206d5f62356f0fd39800
cjun2351 <cjun2351@gmail.com>
2024-02-02 15:16:31 +0800

    修改 table 頁面的 table class 名

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="table">
+  <div class="tables">
     <div class="tableContent">
       <h3>桌次管理</h3>
-      
+
       <form class="tableInfo">
         <div class="tableInfoTitle">
           <span>桌型</span>
@@ -10,29 +10,23 @@
         </div>
         <div class="fourTable">
           <span>4人桌</span>
-          <input type="number" >
+          <input type="number" />
         </div>
         <div class="eightTable">
           <span>8人桌</span>
-          <input type="number" >
+          <input type="number" />
         </div>
         <div class="twelveTable">
           <span>12人桌</span>
-          <input type="number" >
+          <input type="number" />
         </div>
-        <input type="submit" value="儲存設定" class="btn btn-info">
+        <input type="submit" value="儲存設定" class="btn btn-info" />
       </form>
-
-
     </div>
   </div>
 </template>
 
-<script>
-
-
-
-</script>
+<script></script>
 
 <style lang="scss">
 //
