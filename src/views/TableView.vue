40adfde9d7a6762548359b3b6ede5964f14e7d46
smallguo0925 <l09250701@gmail.com>
2024-02-06 12:07:55 +0800

    桌型test

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -3,7 +3,7 @@
     <div class="tableContent">
       <h3>桌次管理</h3>
 
-      <form class="tableInfo">
+      <form class="tableInfo" v-for="(item, index) in table_typeData">
         <div class="tableInfoTitle">
           <span>桌型</span>
           <span>桌次數量</span>
@@ -26,7 +26,31 @@
   </div>
 </template>
 
-<script></script>
+<script>
+  export default{
+    data() {
+      return {
+        table_typeData:[],
+      };
+    },
+    methods: {
+      fetchNews() {
+        axios
+          .post(`${import.meta.env.VITE_API_URL}/admin/table_type.php`, {})
+          .then(res => {
+            console.log(res.data.table_type); //這可以在f12看到自己的陣列，好用！
+            this.table_typeData = res.data.table_type;
+          })
+          .catch(error => console.error('發生錯誤:',error))
+      },
+      
+    },
+    created() {
+      this.fetchNews();
+    }
+  }
+
+</script>
 
 <style lang="scss">
 //
