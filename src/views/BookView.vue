8861ca3adccc6dc49c19db44a542b9e165dbd64c
Mia-Peng <carefree31302@gmail.com>
2024-02-07 10:24:09 +0800

    預約行事曆

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="book">
-
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item" aria-current="page">
+          <router-link to="/home">首頁</router-link>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">預約管理</li>
+        <li class="breadcrumb-item active" aria-current="page">預約訂單</li>
+      </ol>
+    </nav>
+    <div class="titleGroup">
+      <h1>預約訂單</h1>
+      <div class="searchGroup">
+        <select id="searchFilter" class="rounded border border-1 border-dark">
+          <option value="bookId">預訂編號</option>
+          <option value="memId">會員編號</option>
+        </select>
+        <input
+          type="text"
+          id="searchBar"
+          placeholder="請輸入編號"
+          class="rounded border border-1 border-dark"
+        />
+      </div>
+    </div>
     <div class="bookTable">
       <table class="table table-hover">
         <thead>
@@ -16,16 +39,16 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="book in bookData" class="border-bottom text-center">
-            <th class="pb-3 pt-3">{{ book.bookId }}</th>
-            <td>{{ book.memId }}</td>
-            <td>{{ book.memName }}</td>
-            <td>{{ book.bookDate }}</td>
-            <td>{{ book.booktime }}</td>
-            <td>{{ book.numOfPeople }}</td>
-            <td>{{ book.tableType }}</td>
+          <tr v-for="book in booksData" class="border-bottom text-center">
+            <th class="pb-3 pt-3">{{ book.book_id }}</th>
+            <td>{{ book.mem_id }}</td>
+            <td>{{ book.mem_name }}</td>
+            <td>{{ book.book_date}}</td>
+            <td>{{ book.book_time }}</td>
+            <td>{{ book.book_people }}</td>
+            <td>{{ book.table_type_name }}</td>
             <td>
-              <select name="" id="" class="form-select form-select-sm" aria-label="Small select example" v-model="book.bookStatus">
+              <select name="" id="" class="form-select form-select-sm" aria-label="Small select example" v-model="book.book_state" @change="deliverBook">
                 <option value="0">已預定</option>
                 <option value="1">已到場</option>
                 <option value="2">逾時未到</option>
@@ -35,40 +58,35 @@
         </tbody>
       </table>
     </div>
-    <h3>
-    </h3>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      bookData: [
-        {
-          bookId: 1,
-          memId: "22",
-          memName: "陳雅婷",
-          bookDate: '2024-01-22',
-          booktime: '下午12:00-15:00',
-          numOfPeople: 4,
-          tableType: '4人桌',
-          bookStatus: 2,
-        },
-        {
-          bookId: 2,
-          memId: "25",
-          memName: "張教官",
-          bookDate: '2024-02-12',
-          booktime: '上午09:00-12:00',
-          numOfPeople: 8,
-          tableType: '8人桌',
-          bookStatus: 0,
-        },
-      ],
+      booksData: [],
     };
   },
   components: {},
+  created(){
+    this.fetchbooks();
+  },
   mounted() {},
+  methods: {
+    fetchbooks() {
+      axios
+      .post(`${import.meta.env.VITE_API_URL}/book.php`, {})
+      .then(res => {
+        console.log(res.data.books);
+        this.booksData = res.data.books;
+      })
+      .catch(error => console.error('發生錯誤:',error))
+    },
+    deliverBook(){
+      
+    }
+  }
 };
 </script>
 <style lang="scss">
