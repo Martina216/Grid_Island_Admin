838f7e7207d4dc5257b67bdc04a34da2bb287728
martina216 <10230260c@gmail.com>
2024-02-23 20:37:44 +0800

    商品管理 頁數切10個商品 新增

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="product">
+  <main class="product" ref="productListContainer">
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb">
         <li class="breadcrumb-item" aria-current="page">
@@ -67,7 +67,7 @@
         </thead>
         <tbody>
           <tr
-            v-for="item in productDisData"
+            v-for="item in currentProducts"
             class="border-bottom text-left align-middle"
           >
             <th class="pb-3 pt-3 text-center number text-center">
@@ -118,6 +118,34 @@
         </tbody>
       </table>
     </div>
+    <div class="pageBtnList" v-if="!nodata">
+          <button
+            class="pageBtn pageBtncursor"
+            @click="nextPrevPage('prev')"
+            v-if="currentPage != 1"
+          >
+            ＜
+          </button>
+          <button
+            class="pageBtn"
+            v-for="page in totalPages"
+            :key="page"
+            @click="currentPage !== page ? changePage(page) : null"
+            :class="{
+              pageBtncursor: currentPage != page,
+              currPageBtn: currentPage == page,
+            }"
+          >
+            {{ page }}
+          </button>
+          <button
+            class="pageBtn pageBtncursor"
+            @click="nextPrevPage('next')"
+            v-if="currentPage != totalPages"
+          >
+            ＞
+          </button>
+        </div>
     <div class="nodata" v-if="nodata">
       <span>查無資料</span>
     </div>
@@ -153,6 +181,8 @@ export default {
       showAdd: false,
       showEdit: false,
       selectedProd: null,
+      currentPage:1,
+      itemsPerPage:10,
     };
   },
   components: {
@@ -326,10 +356,40 @@ export default {
         console.error(`找不到 id 為 ${prodId} 的商品`);
       }
     },
+    changePage(page) {
+      this.currentPage = page;
+      this.scrollToTop();
+    },
+    nextPrevPage(method) {
+      if (method == "prev") {
+        this.currentPage -= 1;
+      } else {
+        this.currentPage += 1;
+      }
+      this.scrollToTop();
+    },
+    scrollToTop() {
+      const productListContainer = this.$refs.productListContainer;
+      // 確保 productListContainer 不為空
+      if (productListContainer) {
+        // 使用 scrollIntoView 方法將商品列表的頂部滾動到最上面
+        productListContainer.scrollIntoView({
+          behavior: "smooth", // 讓滾動具有平滑效果
+        });
+      }
+    },
   },
   computed: {
     nodata() {
       return this.productDisData.length == 0;
+    },
+    totalPages() {
+      return Math.ceil(this.productDisData.length / this.itemsPerPage);
+    },
+    currentProducts() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.productDisData.slice(start, end);
     },
   },
 };
