13c751c5ea615e08aaeae423c58cb70ccc83652b
martina216 <10230260c@gmail.com>
2024-03-01 14:49:44 +0800

    後台登入頁RWD 修改

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,8 @@
 <template>
   <main class="login">
     <div class="warning">
-      <span>想一覽Griddy的機密嗎？使用 1024 * 768 以上之螢幕解析度的瀏覽器！</span>
+      <span>想一覽Griddy的機密嗎？</span>
+      <span>請使用 1024 * 768 以上之螢幕解析度的瀏覽器！</span>
     </div>
     <div class="wrrap">
       <div class="logo">
